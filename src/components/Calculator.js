eb11a274582ab6f95cf985533f4cdbc415bf9d30
SoniaKarungi <98121137+SoniaKarungi@users.noreply.github.com>
2022-05-24 20:36:01 +0300

    Update Calculator.js
    - Refactor component to function component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,56 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from './Button';
 import calculate from '../logic/calculate';
 
 import '../css/calculator.css';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { object: { total: null, next: null, operation: null } };
-    this.handleClick = this.handleClick.bind(this);
-  }
+function Calculator() {
+  const [calcObject, setCalcObject] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick(buttonName) {
-    const { object } = this.state;
-    const newObject = {
-      ...object,
-      ...calculate(object, buttonName),
-    };
-    this.setState({ object: newObject });
-  }
+  useEffect(() => {
+    const responseElement = document.getElementById('results');
+    const { total, next } = calcObject;
+    responseElement.textContent = next || total || 0;
+  });
 
-  render() {
-    const { object } = this.state;
-    const response = object.next || object.total;
+  const handleClick = (buttonName) => {
+    setCalcObject((prevState) => {
+      const newCalculatorObject = {
+        ...prevState,
+        ...calculate(prevState, buttonName),
+      };
+      return newCalculatorObject;
+    });
+  };
 
-    return (
-      <div className="container">
-        <div className="calc-container">
-          <div className="screen">{response}</div>
-          <Button cssClassName="btn" buttonText="AC" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="+/-" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="%" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="÷" orangeClassName="orange" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="7" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="8" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="9" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="x" orangeClassName="orange" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="4" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="5" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="6" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="-" orangeClassName="orange" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="1" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="2" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="3" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="+" orangeClassName="orange" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="0" orangeClassName="zero" onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="." onButtonClick={this.handleClick} />
-          <Button cssClassName="btn" buttonText="=" orangeClassName="orange" onButtonClick={this.handleClick} />
-        </div>
+  return (
+    <div className="container">
+      <div className="calc-container">
+        <div className="screen" id="results">Results Here...</div>
+        <Button cssClassName="btn" buttonText="AC" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="+/-" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="%" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="÷" orangeClassName="orange" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="7" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="8" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="9" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="x" orangeClassName="orange" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="4" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="5" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="6" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="-" orangeClassName="orange" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="1" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="2" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="3" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="+" orangeClassName="orange" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="0" orangeClassName="zero" onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="." onButtonClick={handleClick} />
+        <Button cssClassName="btn" buttonText="=" orangeClassName="orange" onButtonClick={handleClick} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Calculator;
